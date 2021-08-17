225db33ab5efdefec1ff7a1d803dd421ed1ab91f
Branden Palmer <brandenpalmer08@gmail.com>
2021-08-17 14:43:31 +0700

    add the four basic cal functions

---
+++ script.js
@@ -0,0 +1,19 @@
+// add two numbers
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+// subtract two numbers
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+// multiply two numbers
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+// divide two numbers
+function divide(num1, num2) {
+  num2 == 0 ? "add error msg to div func" : num1 / num2;
+}
