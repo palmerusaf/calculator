24e0832987198f597ac0a8b7152cb403834a7f33
Branden Palmer <brandenpalmer08@gmail.com>
2021-08-18 18:23:11 +0700

    add tested function feedDisplayDigits that add digits to display

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// add two numbers
+/*** add two numbers*/
 function add(num1, num2) {
   return num1 + num2;
 }
 
-// subtract two numbers
+/** subtract two numbers */
 function subtract(num1, num2) {
   return num1 - num2;
 }
 
-// multiply two numbers
+/** multiply two numbers */
 function multiply(num1, num2) {
   return num1 * num2;
 }
 
-// divide two numbers
+/** divide two numbers */
 function divide(num1, num2) {
   if (num2 == 0) return "ERROR: Division by zero not supported at this time.";
   return num1 / num2;
 }
 
-// get two numbers, string operator, and select one of the four basic operations above
+/** get two numbers, string operator, and select one of four basic math operations */
 function operationSelection(num1, opStr, num2) {
   switch (opStr) {
     case "+":
@@ -34,7 +34,8 @@ function operationSelection(num1, opStr, num2) {
       return "ERROR: Operation Selection error.";
   }
 }
-// opsSelection test
+// opsSelection tests
+/** 
 console.log(operationSelection(4, "+", 4));
 console.log(operationSelection(4, "-", 4));
 console.log(operationSelection(4, "*", 4));
@@ -43,3 +44,33 @@ console.log(operationSelection(1, "/", 4));
 console.log(operationSelection(4, "/", 0));
 console.log(operationSelection(4, "f", 0));
 console.log(operationSelection(4, "f", 1));
+ */
+
+/** get the number from digit buttons on button click */
+function getDigitButtonInput(){
+
+}
+
+/** feed input from digit buttons into display */
+function feedDisplayDigits(digit){
+const display = document.querySelector("#numDisplay");
+display.textContent+=`${digit}`;
+}
+// feedDisplayDigits tests
+/* 
+feedDisplayDigits(0);
+feedDisplayDigits(1);
+feedDisplayDigits(2);
+feedDisplayDigits(3);
+feedDisplayDigits(4);
+feedDisplayDigits(5);
+feedDisplayDigits(6);
+feedDisplayDigits(7);
+feedDisplayDigits(8);
+feedDisplayDigits(9);
+feedDisplayDigits(42);
+// */
+
+/** When math function button is pressed add space, math function symbol, and space to display. */
+
+/** when equal is pressed get data from display pass to ops selection */
